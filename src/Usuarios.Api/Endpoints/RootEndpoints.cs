46c8a31b8b19b38243ab1cfdf9e73785248b5edd
Felipe Augusto <felipementel@hotmail.com>
2026-06-28 11:46:10 -0300

    Update RootEndpoints.cs

--- src/Usuarios.Api/Endpoints/RootEndpoints.cs
+++ src/Usuarios.Api/Endpoints/RootEndpoints.cs
@@ -18,7 +18,7 @@ public static class RootEndpoints
                 <head>
                   <meta charset="UTF-8" />
                   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                  <title>Usuarios API</title>
+                  <title>API Details</title>
                   <style>
                     *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
                     body {
